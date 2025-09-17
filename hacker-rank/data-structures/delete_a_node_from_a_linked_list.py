b537d5d547925dfd24031b3e7315705660fa4d09
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2025-09-16 18:28:42 -0600

    hr: ds: delete a node from a linked list

---
+++ hacker-rank/data-structures/delete_a_node_from_a_linked_list.py
@@ -0,0 +1,81 @@
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if self.head:
+            self.tail.next = node
+        else:
+            self.head = node
+
+        self.tail = node
+
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+
+#
+# Complete the 'deleteNode' function below.
+#
+# The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+# The function accepts following parameters:
+#  1. INTEGER_SINGLY_LINKED_LIST llist
+#  2. INTEGER position
+#
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+
+def deleteNode(head, position):
+    if position == 0:
+        return head.next
+
+    current = head
+    for _ in range(position - 1):
+        current = current.next
+
+    current.next = current.next.next
+    return head
+
+
+if __name__ == '__main__':
+    fptr = open('tmp.txt', 'w')
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist.insert_node(llist_item)
+
+    position = int(input())
+
+    llist1 = deleteNode(llist.head, position)
+
+    print_singly_linked_list(llist1, ' ', fptr)
+    fptr.write('\n')
+
+    fptr.close()
