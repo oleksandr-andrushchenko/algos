8529f53761fdb145dbd69d79aaee8c2111b4f209
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2025-09-16 18:55:27 -0600

    hr: ds: reverse a linked list

---
+++ hacker-rank/data-structures/reverse_a_linked_list.py
@@ -0,0 +1,82 @@
+# Given the pointer to the head node of a linked list, change the next pointers of the nodes so that their order is reversed.
+# The head pointer given may be null meaning that the initial list is empty.
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+        self.tail = node
+
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+
+#
+# Complete the 'reverse' function below.
+#
+# The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+# The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
+#
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+
+def reverse(head):
+    prev = None
+    current = head
+    while current:
+        nxt = current.next  # store next node
+        current.next = prev  # reverse the link
+        prev = current  # move prev forward
+        current = nxt  # move current forward
+    return prev  # prev becomes new head
+
+
+if __name__ == '__main__':
+    fptr = open('tmp.txt', 'w')
+
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        llist_count = int(input())
+
+        llist = SinglyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        llist1 = reverse(llist.head)
+
+        print_singly_linked_list(llist1, ' ', fptr)
+        fptr.write('\n')
+
+    fptr.close()
