8d444a25ceffd6224e8244b7d7e169a2afa0b3a6
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2025-09-16 18:38:23 -0600

    hr: ds: print the elements of a linked list in reverse

---
+++ hacker-rank/data-structures/print_the_elements_of_a_linked_list_in_reverse.py
@@ -0,0 +1,68 @@
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if self.head:
+            self.tail.next = node
+        else:
+            self.head = node
+
+        self.tail = node
+
+
+def print_singly_linked_list(node, sep):
+    while node:
+        print(node.data, end='')
+
+        node = node.next
+
+        if node:
+            print(sep, end='')
+
+
+#
+# Complete the 'reversePrint' function below.
+#
+# The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
+#
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+
+def reversePrint(head):
+    stack = []
+    while head:
+        stack.append(head.data)
+        head = head.next
+
+    for c in reversed(stack):
+        print(c)
+
+
+if __name__ == '__main__':
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        llist_count = int(input())
+
+        llist = SinglyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        reversePrint(llist.head)
