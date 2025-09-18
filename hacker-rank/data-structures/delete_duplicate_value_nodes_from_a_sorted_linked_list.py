9b172fb3017df5637de64d88c63d1ca792e4ddd1
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2025-09-18 11:42:09 -0600

    hr: ds: delete duplicate value nodes from a sorted linked list

---
+++ hacker-rank/data-structures/delete_duplicate_value_nodes_from_a_sorted_linked_list.py
@@ -0,0 +1,82 @@
+# You are given the pointer to the head node of a sorted linked list, where the data in the nodes is in ascending order.
+# Delete nodes and return a sorted list with each distinct value in the original list.
+# The given head pointer may be null indicating that the list is empty.
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+        self.tail = node
+
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+
+#
+# Complete the 'removeDuplicates' function below.
+#
+# The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+# The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
+#
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+
+def removeDuplicates(head):
+    current = head
+    while current.next:
+        if current.data == current.next.data:
+            current.next = current.next.next
+        else:
+            current = current.next
+    return head
+
+
+if __name__ == '__main__':
+    fptr = open('tmp.txt', 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        llist_count = int(input())
+
+        llist = SinglyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        llist1 = removeDuplicates(llist.head)
+
+        print_singly_linked_list(llist1, ' ', fptr)
+        fptr.write('\n')
+
+    fptr.close()
