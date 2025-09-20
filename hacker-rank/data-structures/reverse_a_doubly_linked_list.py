69f1166bdeb7e7be2900ae331af166974b7d59d1
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2025-09-19 23:13:33 -0600

    hr: ds: reverse a doubly linked list

---
+++ hacker-rank/data-structures/reverse_a_doubly_linked_list.py
@@ -0,0 +1,87 @@
+# Given the pointer to the head node of a doubly linked list, reverse the order of the nodes in place.
+# That is, change the next and prev pointers of the nodes so that the direction of the list is reversed.
+# Return a reference to the head node of the reversed list.
+
+class DoublyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+        self.prev = None
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = DoublyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+
+        self.tail = node
+
+
+def print_doubly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+
+#
+# Complete the 'reverse' function below.
+#
+# The function is expected to return an INTEGER_DOUBLY_LINKED_LIST.
+# The function accepts INTEGER_DOUBLY_LINKED_LIST llist as parameter.
+#
+
+#
+# For your reference:
+#
+# DoublyLinkedListNode:
+#     int data
+#     DoublyLinkedListNode next
+#     DoublyLinkedListNode prev
+#
+#
+
+def reverse(head):
+    prev = None
+    current = head
+    while current:
+        nxt = current.next  # store next node
+        current.next = prev  # reverse the link
+        current.prev = nxt
+        prev = current  # move prev forward
+        current = nxt  # move current forward
+    return prev  # prev becomes new head
+
+
+if __name__ == '__main__':
+    fptr = open('tmp.txt', 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        llist_count = int(input())
+
+        llist = DoublyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        llist1 = reverse(llist.head)
+
+        print_doubly_linked_list(llist1, ' ', fptr)
+        fptr.write('\n')
+
+    fptr.close()
