01b4c8c0d692a2f0ca034ca161c15b3540017c70
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2025-09-20 15:41:24 -0600

    hr: ds: tree height of a binary tree

---
+++ hacker-rank/data-structures/tree_height_of_a_binary_tree.py
@@ -0,0 +1,75 @@
+# The height of a binary tree is the number of edges between the tree's root and its furthest leaf.
+# For example, the following binary tree is of height :
+
+# Complete the getHeight or height function in the editor. It must return the height of a binary tree as an integer.
+#
+# getHeight or height has the following parameter(s):
+
+class Node:
+    def __init__(self, info):
+        self.info = info
+        self.left = None
+        self.right = None
+        self.level = None
+
+    def __str__(self):
+        return str(self.info)
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def create(self, val):
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+'''
+class Node:
+      def __init__(self,info): 
+          self.info = info  
+          self.left = None  
+          self.right = None 
+
+
+       // this is a node of the tree , which contains info as data, left , right
+'''
+
+
+def height(node):
+    if node is None:
+        return -1
+    left_height = height(node.left)
+    right_height = height(node.right)
+    return 1 + max(left_height, right_height)
+
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+print(height(tree.root))
