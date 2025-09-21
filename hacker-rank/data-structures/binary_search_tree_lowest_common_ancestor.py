84aa313fa3b43aca111fc0246275c8069b33b5e2
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2025-09-21 07:52:56 -0600

    hr: ds: binary search tree lowest common ancestor

---
+++ hacker-rank/data-structures/binary_search_tree_lowest_common_ancestor.py
@@ -0,0 +1,77 @@
+# You are given pointer to the root of the binary search tree and two values  and .
+# You need to return the lowest common ancestor (LCA) of  and  in the binary search tree.
+
+class Node:
+    def __init__(self, info):
+        self.info = info
+        self.left = None
+        self.right = None
+        self.level = None
+
+    def __str__(self):
+        return str(self.info)
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def create(self, val):
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+'''
+class Node:
+      def __init__(self,info): 
+          self.info = info  
+          self.left = None  
+          self.right = None 
+
+
+       // this is a node of the tree , which contains info as data, left , right
+'''
+
+
+def lca(root, v1, v2):
+    current = root
+    while current:
+        if v1 > current.info and v2 > current.info:
+            current = current.right
+        elif v1 < current.info and v2 < current.info:
+            current = current.left
+        else:
+            return current
+
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+v = list(map(int, input().split()))
+
+ans = lca(tree.root, v[0], v[1])
+print(ans.info)
