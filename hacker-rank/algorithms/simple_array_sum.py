138ee17e1826a47114f17402efb9ef8258e01bef
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2025-09-21 21:16:35 -0600

    hr: ds: simple array sum

---
+++ hacker-rank/algorithms/simple_array_sum.py
@@ -0,0 +1,25 @@
+# Given an array of integers, find the sum of its elements.
+
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+def simpleArraySum(ar):
+    return sum(ar)
+
+
+if __name__ == '__main__':
+    fptr = open('tmp.txt', 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
