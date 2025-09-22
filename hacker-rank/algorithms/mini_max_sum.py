f563d8fc39aead5e4e77248a35ef22bdc9a26543
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2025-09-21 21:36:31 -0600

    hr: ar: mini max sum

---
+++ hacker-rank/algorithms/mini_max_sum.py
@@ -0,0 +1,22 @@
+# Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers.
+# Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+
+def miniMaxSum(arr):
+    total = sum(arr)
+    min_sum = total - max(arr)
+    max_sum = total - min(arr)
+
+    print(f"{min_sum} {max_sum}")
+
+
+if __name__ == '__main__':
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
