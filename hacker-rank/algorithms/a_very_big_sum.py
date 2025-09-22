99ca48cd8fc2ab5413b7911597a7d0d59530b822
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2025-09-21 21:20:41 -0600

    hr: ds: a very big sum

---
+++ hacker-rank/algorithms/a_very_big_sum.py
@@ -0,0 +1,25 @@
+# In this challenge, you need to calculate and print the sum of elements in an array, considering that some integers may be very large.
+
+#
+# Complete the 'aVeryBigSum' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER_ARRAY ar as parameter.
+#
+
+def aVeryBigSum(ar):
+    return sum(ar)
+
+
+if __name__ == '__main__':
+    fptr = open('tmp.txt', 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
