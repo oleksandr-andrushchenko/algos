078492ff402638fec91c89b99156b89b23980e72
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2025-09-22 21:57:19 -0600

    hr: sql: symmetric pairs

---
+++ hacker-rank/sql/symmetric_pairs.sql
@@ -0,0 +1,14 @@
+-- You are given a table, Functions, containing two columns: X and Y.
+
+-- Two pairs (X1, Y1) and (X2, Y2) are said to be symmetric pairs if X1 = Y2 and X2 = Y1.
+--
+-- Write a query to output all such symmetric pairs in ascending order by the value of X. List the rows such that X1 ≤ Y1.
+
+SELECT f1.X, f1.Y
+FROM Functions f1
+         JOIN Functions f2
+              ON f1.X = f2.Y AND f1.Y = f2.X
+GROUP BY f1.X, f1.Y
+HAVING (f1.X < f1.Y)
+    OR (f1.X = f1.Y AND COUNT(*) > 1)
+ORDER BY f1.X;
