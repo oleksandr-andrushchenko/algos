05333e4b409dd260c843b5b1e7d7e21ea93cd150
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2025-09-24 10:29:46 -0600

    hr: c: sum numbers

---
+++ hacker-rank/c/sum_numbers_c.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a, b;
+    float x, y;
+
+    // Read two integers
+    scanf("%d %d", &a, &b);
+
+    // Read two floats
+    scanf("%f %f", &x, &y);
+
+    // Print sum and difference of integers
+    printf("%d %d\n", a + b, a - b);
+
+    // Print sum and difference of floats, rounded to 1 decimal place
+    printf("%.1f %.1f\n", x + y, x - y);
+
+    return 0;
+}
