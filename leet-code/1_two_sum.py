af5cb2b4a4b6701a05260102878c026bc64e6748
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2025-09-26 09:19:15 -0600

    lc: 1: two sum

---
+++ leet-code/1_two_sum.py
@@ -0,0 +1,18 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+#
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+#
+# You can return the answer in any order.
+
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        diff_sums = {}
+
+        for i in range(len(nums)):
+            if nums[i] in diff_sums:
+                return [i, diff_sums[nums[i]]]
+
+            diff_sums[target - nums[i]] = i
