0117bba55dca2cea340d02fbdfd76499b0b9fc74
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2025-10-02 21:32:14 -0600

    lc: 9: palindrome number

---
+++ leet-code/9_palindrome_number.py
@@ -0,0 +1,13 @@
+# Given an integer x, return true if x is a palindrome, and false otherwise.
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        s = str(x)
+        left, right = 0, len(s) - 1
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+
+        return True
