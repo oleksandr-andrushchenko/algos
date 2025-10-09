ada6d448ef00b8fb682978777e55bffcd6040bb3
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2025-10-09 08:45:26 -0600

    lc: 145: binary tree postorder traversal

---
+++ leet-code/145_binary_tree_postorder_traversal.py
@@ -0,0 +1,27 @@
+# Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+from typing import Optional, List
+
+
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+
+        def traverse(node):
+            if not node:
+                return
+            traverse(node.left)
+            traverse(node.right)
+            res.append(node.val)
+
+        traverse(root)
+
+        return res
