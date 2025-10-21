d3b7291399076e7742c08825fc4ee558e4155b37
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2025-10-21 00:04:03 -0600

    lc: 206: reverse linked list

---
+++ leet-code/206_reverse_linked_list.py
@@ -0,0 +1,24 @@
+# Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+# Definition for singly-linked list.
+
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        cur = head
+        while cur:
+            nxt = cur.next
+            cur.next = prev
+            prev = cur
+            cur = nxt
+
+        return prev
