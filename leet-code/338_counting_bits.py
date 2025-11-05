fc92b0d2215f7994cc19b0f91af39f89f8c1fc0b
oleksandr.andrushchenko1988@gmail.com <oleksandr.andrushchenko1988@gmail.com>
2025-11-04 18:35:56 -0700

    lc: 338: counting bits

---
+++ leet-code/338_counting_bits.py
@@ -0,0 +1,12 @@
+# Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n),
+# ans[i] is the number of 1's in the binary representation of i.
+
+from typing import List
+
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans = [0] * (n + 1)
+        for i in range(1, n + 1):
+            ans[i] = ans[i >> 1] + (i & 1)
+        return ans
