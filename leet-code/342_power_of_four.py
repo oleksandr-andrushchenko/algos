dceb004b1371de8a9d37277b541a3d109814ad1f
oleksandr.andrushchenko1988@gmail.com <oleksandr.andrushchenko1988@gmail.com>
2025-11-05 18:41:23 -0700

    lc: 342: power of four

---
+++ leet-code/342_power_of_four.py
@@ -0,0 +1,11 @@
+# Given an integer n, return true if it is a power of four. Otherwise, return false.
+#
+# An integer n is a power of four, if there exists an integer x such that n == 4x.
+
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n < 1:
+            return False
+        while n % 4 == 0:
+            n //= 4
+        return n == 1
