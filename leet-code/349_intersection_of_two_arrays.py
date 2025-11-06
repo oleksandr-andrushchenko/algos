ac70d4fdfc924e066d8c27860296103dd8708aac
oleksandr.andrushchenko1988@gmail.com <oleksandr.andrushchenko1988@gmail.com>
2025-11-06 09:13:15 -0700

    lc: 349: intersection of two arrays

---
+++ leet-code/349_intersection_of_two_arrays.py
@@ -0,0 +1,8 @@
+# Given two integer arrays nums1 and nums2, return an array of their intersection.
+# Each element in the result must be unique and you may return the result in any order.
+from typing import List
+
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return list(set(nums1) & set(nums2))
