85ae000ad63ef6860a38a48032cbc9449e432db3
oleksandr.andrushchenko1988@gmail.com <oleksandr.andrushchenko1988@gmail.com>
2025-11-06 08:49:44 -0700

    lc: 345: reverse vowels of a string

---
+++ leet-code/345_reverse_vowels_of_a_string.py
@@ -0,0 +1,21 @@
+# Given a string s, reverse only all the vowels in the string and return it.
+#
+# The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        vowels = set('aeiouAEIOU')
+        s = list(s)
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            while left < right and s[left] not in vowels:
+                left += 1
+            while left < right and s[right] not in vowels:
+                right -= 1
+
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+
+        return ''.join(s)
