e9a43b1734237eb44735328c8d581d12fc24cdbf
oleksandr.andrushchenko1988@gmail.com <oleksandr.andrushchenko1988@gmail.com>
2025-11-09 20:26:08 -0700

    lc : 386: lexicographical numbers

---
+++ leet-code/386_lexicographical_numbers.py
@@ -0,0 +1,10 @@
+# Given an integer n, return all the numbers in the range [1, n] sorted in lexicographical order.
+#
+# You must write an algorithm that runs in O(n) time and uses O(1) extra space.
+
+from typing import List
+
+
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        pass
