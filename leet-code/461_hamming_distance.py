9368a1efa7b6d01fe5310edfc21d307c6ff26eeb
oleksandr.andrushchenko1988@gmail.com <oleksandr.andrushchenko1988@gmail.com>
2025-11-18 22:13:11 -0700

    lc: 461: hamming distance

---
+++ leet-code/461_hamming_distance.py
@@ -0,0 +1,7 @@
+# The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+#
+# Given two integers x and y, return the Hamming distance between them.
+
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        return bin(x ^ y).count("1")
