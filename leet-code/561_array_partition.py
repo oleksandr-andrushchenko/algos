c50b9140dfe862d0a1993ab118e9fbc273c4bb29
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2025-12-02 20:50:35 -0700

    lc: 561: array partition

---
+++ leet-code/561_array_partition.py
@@ -0,0 +1,7 @@
+# Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2), ..., (an, bn) such
+# that the sum of min(ai, bi) for all i is maximized. Return the maximized sum.
+
+class Solution:
+    def arrayPairSum(self, nums: list[int]) -> int:
+        nums.sort()
+        return sum(nums[::2])
