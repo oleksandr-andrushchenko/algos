43868829d21b7bf9d8acf152cb4e45360b0287f4
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-01-01 18:01:38 -0700

    lc: 783: minimum distance between bst nodes

---
+++ leet-code/easy/701-800/783_minimum_distance_between_bst_nodes.py
@@ -0,0 +1,12 @@
+# Given the root of a Binary Search Tree (BST), return the minimum difference between the values of any two different nodes in the tree.
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def minDiffInBST(self, root: TreeNode | None) -> int:
+        pass
