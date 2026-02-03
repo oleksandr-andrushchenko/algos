223c3a5d641c5eb6f0321ee6ce0191d245ce71b9
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-02-02 19:47:54 -0700

    lc: 1047: remove all adjacent duplicates in string

---
+++ leet-code/easy/1001-1100/1047_remove_all_adjacent_duplicates_in_string.py
@@ -0,0 +1,16 @@
+# You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two
+# adjacent and equal letters and removing them.
+#
+# We repeatedly make duplicate removals on s until we no longer can.
+#
+# Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique.
+
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        stack = []
+        for char in s:
+            if stack and stack[-1] == char:
+                stack.pop()  # remove the duplicate pair
+            else:
+                stack.append(char)
+        return "".join(stack)
