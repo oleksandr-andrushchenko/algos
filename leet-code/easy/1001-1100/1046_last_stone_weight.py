4e50c27b0b11119e5df44f60a8788b43f20d12e9
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-02-02 19:44:18 -0700

    lc: 1046: last stone weight

---
+++ leet-code/easy/1001-1100/1046_last_stone_weight.py
@@ -0,0 +1,28 @@
+# You are given an array of integers stones where stones[i] is the weight of the ith stone.
+#
+# We are playing a game with the stones. On each turn, we choose the heaviest two stones and smash them together.
+# Suppose the heaviest two stones have weights x and y with x <= y. The result of this smash is:
+#
+# If x == y, both stones are destroyed, and
+# If x != y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x.
+# At the end of the game, there is at most one stone left.
+#
+# Return the weight of the last remaining stone. If there are no stones left, return 0.
+
+import heapq
+
+
+class Solution:
+    def lastStoneWeight(self, stones: list[int]) -> int:
+        # Create a max-heap by storing negative weights
+        max_heap = [-stone for stone in stones]
+        heapq.heapify(max_heap)
+
+        # Smash stones until 0 or 1 stone is left
+        while len(max_heap) > 1:
+            first = -heapq.heappop(max_heap)  # heaviest
+            second = -heapq.heappop(max_heap)  # second heaviest
+            if first != second:
+                heapq.heappush(max_heap, -(first - second))
+
+        return -max_heap[0] if max_heap else 0
