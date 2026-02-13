6ed30a77dce302f846a43f78dfe8c752bee17cec
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-02-12 18:52:07 -0700

    lc: 1137: n-th tribonacci number

---
+++ leet-code/easy/1101-1200/1137_n_th_tribonacci_number.py
@@ -0,0 +1,19 @@
+# The Tribonacci sequence Tn is defined as follows:
+#
+# T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+#
+# Given n, return the value of Tn.
+
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1 or n == 2:
+            return 1
+
+        a, b, c = 0, 1, 1  # T0, T1, T2
+
+        for _ in range(3, n + 1):
+            a, b, c = b, c, a + b + c
+
+        return c
