b32968ce3d5c7fe9255925140e82e3df1db8b2a4
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-02-12 18:50:38 -0700

    lc: 1128: number of equivalent domino pairs

---
+++ leet-code/easy/1101-1200/1128_number_of_equivalent_domino_pairs.py
@@ -0,0 +1,19 @@
+# Given a list of dominoes, dominoes[i] = [a, b] is equivalent to dominoes[j] = [c, d] if and only if either
+# (a == c and b == d), or (a == d and b == c) - that is, one domino can be rotated to be equal to another domino.
+#
+# Return the number of pairs (i, j) for which 0 <= i < j < dominoes.length, and dominoes[i] is equivalent to dominoes[j].
+
+from collections import defaultdict
+
+
+class Solution:
+    def numEquivDominoPairs(self, dominoes: list[list[int]]) -> int:
+        count = defaultdict(int)
+        result = 0
+
+        for a, b in dominoes:
+            key = (min(a, b), max(a, b))
+            result += count[key]
+            count[key] += 1
+
+        return result
