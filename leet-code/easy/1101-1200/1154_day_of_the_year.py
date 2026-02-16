d5d98aab5312e1270c06c5e974ef1bbd938b5518
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-02-15 18:08:09 -0700

    lc: 1154: day of the year

---
+++ leet-code/easy/1101-1200/1154_day_of_the_year.py
@@ -0,0 +1,16 @@
+# Given a string date representing a Gregorian calendar date formatted as YYYY-MM-DD, return the day number of the year.
+
+class Solution:
+    def dayOfYear(self, date: str) -> int:
+        year, month, day = map(int, date.split('-'))
+
+        # Days in each month (non-leap year)
+        days_in_month = [31, 28, 31, 30, 31, 30,
+                         31, 31, 30, 31, 30, 31]
+
+        # Check for leap year
+        if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+            days_in_month[1] = 29
+
+        # Sum days of previous months + current day
+        return sum(days_in_month[:month - 1]) + day
