abf8490cdb191a15674dc7adcae1a8d954fc010a
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-03-01 20:40:14 -0700

    lc: 1309: decrypt-string-from-alphabet-to-integer-mapping

---
+++ leet-code/easy/1301-1400/1309-decrypt-string-from-alphabet-to-integer-mapping.py
@@ -0,0 +1,24 @@
+# You are given a string s formed by digits and '#'. We want to map s to English lowercase characters as follows:
+#
+# Characters ('a' to 'i') are represented by ('1' to '9') respectively.
+# Characters ('j' to 'z') are represented by ('10#' to '26#') respectively.
+# Return the string formed after mapping.
+#
+# The test cases are generated so that a unique mapping will always exist.
+
+class Solution:
+    def freqAlphabets(self, s: str) -> str:
+        result = []
+        i = len(s) - 1
+
+        while i >= 0:
+            if s[i] == '#':
+                num = int(s[i - 2:i])
+                result.append(chr(ord('a') + num - 1))
+                i -= 3
+            else:
+                num = int(s[i])
+                result.append(chr(ord('a') + num - 1))
+                i -= 1
+
+        return ''.join(reversed(result))
