baa97ee03e2323bd131f1c75ff504b3d44020788
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-03-17 14:39:43 -0600

    lc: 1460: make-two-arrays-equal-by-reversing-subarrays

---
+++ leet-code/easy/1401-1500/1460-make-two-arrays-equal-by-reversing-subarrays.py
@@ -0,0 +1,11 @@
+# You are given two integer arrays of equal length target and arr. In one step, you can select any non-empty subarray
+# of arr and reverse it. You are allowed to make any number of steps.
+#
+# Return true if you can make arr equal to target or false otherwise.
+
+from collections import Counter
+
+
+class Solution:
+    def canBeEqual(self, target: list[int], arr: list[int]) -> bool:
+        return Counter(target) == Counter(arr)
