8def62f9cd8debfefae374848d542a9213dde705
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-03-24 09:30:51 -0600

    lc: 1523: count-odd-numbers-in-an-interval-range

---
+++ leet-code/easy/1501-1600/1523-count-odd-numbers-in-an-interval-range.py
@@ -0,0 +1,5 @@
+# Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive).
+
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        return (high + 1) // 2 - low // 2
