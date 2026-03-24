5363ba041ee879249514bf47d2cdd45bce1bea6e
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-03-24 09:29:48 -0600

    lc: 1518: water-bottles

---
+++ leet-code/easy/1501-1600/1518-water-bottles.py
@@ -0,0 +1,18 @@
+# There are numBottles water bottles that are initially full of water. You can exchange numExchange empty water bottles
+# from the market with one full water bottle.
+#
+# The operation of drinking a full water bottle turns it into an empty bottle.
+#
+# Given the two integers numBottles and numExchange, return the maximum number of water bottles you can drink.
+
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        total_drunk = numBottles
+        empty = numBottles
+
+        while empty >= numExchange:
+            new_bottles = empty // numExchange
+            total_drunk += new_bottles
+            empty = empty % numExchange + new_bottles
+
+        return total_drunk
