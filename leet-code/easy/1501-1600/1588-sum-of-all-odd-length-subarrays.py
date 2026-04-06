2010f5ef7f0636a6a21102f92f363c788d681d4e
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-04-06 14:30:30 -0600

    lc: 1588: sum-of-all-odd-length-subarrays #2

--- leet-code/easy/1501-1600/1588-sum-of-all-odd-length-subarrays.py
+++ leet-code/easy/1501-1600/1588-sum-of-all-odd-length-subarrays.py
@@ -4,3 +4,19 @@
 
 class Solution:
     def sumOddLengthSubarrays(self, arr: list[int]) -> int:
+        n = len(arr)
+        total = 0
+
+        for i in range(n):
+            left = i + 1
+            right = n - i
+
+            # total subarrays that include arr[i]
+            total_subarrays = left * right
+
+            # number of odd-length subarrays including arr[i]
+            odd_count = (total_subarrays + 1) // 2
+
+            total += arr[i] * odd_count
+
+        return total
