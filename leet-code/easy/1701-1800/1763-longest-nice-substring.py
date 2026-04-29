b4f1c68bfdd62eeb42077ea41e2e3490aef0bcc4
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-04-29 17:29:22 -0600

    lc: 1763-longest-nice-substring

---
+++ leet-code/easy/1701-1800/1763-longest-nice-substring.py
@@ -0,0 +1,21 @@
+# A string s is nice if, for every letter of the alphabet that s contains, it appears both in uppercase and lowercase.
+# For example, "abABB" is nice because 'A' and 'a' appear, and 'B' and 'b' appear. However, "abA" is not because 'b'
+# appears, but 'B' does not.
+#
+# Given a string s, return the longest substring of s that is nice. If there are multiple, return the substring of the
+# earliest occurrence. If there are none, return an empty string.
+
+class Solution:
+    def longestNiceSubstring(self, s: str) -> str:
+        if len(s) < 2:
+            return ""
+
+        char_set = set(s)
+
+        for i, c in enumerate(s):
+            if c.swapcase() not in char_set:
+                left = self.longestNiceSubstring(s[:i])
+                right = self.longestNiceSubstring(s[i + 1:])
+                return left if len(left) >= len(right) else right
+
+        return s
