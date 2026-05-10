3de0f18738ee972d8a368e1cbf80aedd5575183d
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-05-09 21:55:41 -0600

    lc: 1837-sum-of-digits-in-base-k

---
+++ leet-code/easy/1801-1900/1837-sum-of-digits-in-base-k.py
@@ -0,0 +1,13 @@
+# Given an integer n (in base 10) and a base k, return the sum of the digits of n after converting n from base 10 to base k.
+#
+# After converting, each digit should be interpreted as a base 10 number, and the sum should be returned in base 10.
+
+class Solution:
+    def sumBase(self, n: int, k: int) -> int:
+        total = 0
+
+        while n > 0:
+            total += n % k
+            n //= k
+
+        return total
