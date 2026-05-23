729d457cc092398506aa6247e3cacd2a0aecc6c7
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-05-22 22:26:36 -0600

    lc: 1957-delete-characters-to-make-fancy-string

---
+++ leet-code/easy/1901-2000/1957-delete-characters-to-make-fancy-string.py
@@ -0,0 +1,17 @@
+# A fancy string is a string where no three consecutive characters are equal.
+#
+# Given a string s, delete the minimum possible number of characters from s to make it fancy.
+#
+# Return the final string after the deletion. It can be shown that the answer will always be unique.
+
+class Solution:
+    def makeFancyString(self, s: str) -> str:
+        result = []
+
+        for char in s:
+            if len(result) >= 2 and result[-1] == result[-2] == char:
+                continue
+
+            result.append(char)
+
+        return ''.join(result)
