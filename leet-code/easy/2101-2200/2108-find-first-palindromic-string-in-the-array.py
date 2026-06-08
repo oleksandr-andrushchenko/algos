a2fc9c87d4a5c5a0b974ceb8e0055901d6b9a457
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-06-08 14:43:52 -0600

    lc: 2108-find-first-palindromic-string-in-the-array

---
+++ leet-code/easy/2101-2200/2108-find-first-palindromic-string-in-the-array.py
@@ -0,0 +1,15 @@
+# Given an array of strings words, return the first palindromic string in the array. If there is no such string, return
+# an empty string "".
+#
+# A string is palindromic if it reads the same forward and backward.
+
+from typing import List
+
+
+class Solution:
+    def firstPalindrome(self, words: List[str]) -> str:
+        for word in words:
+            if word == word[::-1]:
+                return word
+
+        return ""
