6f15e6f8a699848dd53bb119eb7608a0daf3f246
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-06-25 20:29:56 -0500

    lc: 2154-keep-multiplying-found-values-by-two

---
+++ leet-code/easy/2101-2200/2154-keep-multiplying-found-values-by-two.py
@@ -0,0 +1,21 @@
+# You are given an array of integers nums. You are also given an integer original which is the first number that needs
+# to be searched for in nums.
+#
+# You then do the following steps:
+#
+# If original is found in nums, multiply it by two (i.e., set original = 2 * original).
+# Otherwise, stop the process.
+# Repeat this process with the new number as long as you keep finding the number.
+# Return the final value of original.
+
+from typing import List
+
+
+class Solution:
+    def findFinalValue(self, nums: List[int], original: int) -> int:
+        nums_set = set(nums)
+
+        while original in nums_set:
+            original *= 2
+
+        return original
