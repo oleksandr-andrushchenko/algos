7337e659d96f0f8a3dd00a05746c84490f8dc99b
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-07-07 18:41:01 -0500

    lc: 2315-count-asterisks

---
+++ leet-code/easy/2300-2399/2315-count-asterisks.py
@@ -0,0 +1,19 @@
+# You are given a string s, where every two consecutive vertical bars '|' are grouped into a pair. In other words, the
+# 1st and 2nd '|' make a pair, the 3rd and 4th '|' make a pair, and so forth.
+#
+# Return the number of '*' in s, excluding the '*' between each pair of '|'.
+#
+# Note that each '|' will belong to exactly one pair.
+
+class Solution:
+    def countAsterisks(self, s: str) -> int:
+        count = 0
+        inside = False
+
+        for c in s:
+            if c == "|":
+                inside = not inside
+            elif c == "*" and not inside:
+                count += 1
+
+        return count
