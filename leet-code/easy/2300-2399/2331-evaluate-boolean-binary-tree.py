e4872e3d29955f374dd28efd1b81d3e4535a7527
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-07-08 13:31:36 -0500

    lc: 2331-evaluate-boolean-binary-tree

---
+++ leet-code/easy/2300-2399/2331-evaluate-boolean-binary-tree.py
@@ -0,0 +1,39 @@
+# You are given the root of a full binary tree with the following properties:
+#
+# Leaf nodes have either the value 0 or 1, where 0 represents False and 1 represents True.
+# Non-leaf nodes have either the value 2 or 3, where 2 represents the boolean OR and 3 represents the boolean AND.
+# The evaluation of a node is as follows:
+#
+# If the node is a leaf node, the evaluation is the value of the node, i.e. True or False.
+# Otherwise, evaluate the node's two children and apply the boolean operation of its value with the children's evaluations.
+# Return the boolean result of evaluating the root node.
+#
+# A full binary tree is a binary tree where each node has either 0 or 2 children.
+#
+# A leaf node is a node that has zero children.
+
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def evaluateTree(self, root: Optional[TreeNode]) -> bool:
+        if root.val == 0:
+            return False
+
+        if root.val == 1:
+            return True
+
+        left = self.evaluateTree(root.left)
+        right = self.evaluateTree(root.right)
+
+        if root.val == 2:
+            return left or right
+
+        return left and right
