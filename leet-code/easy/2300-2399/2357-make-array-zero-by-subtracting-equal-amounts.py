bb790be0dfcbcfa8b9837ffdb5b04bf37e944dc7
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-07-10 11:19:39 -0500

    lc: 2357-make-array-zero-by-subtracting-equal-amounts

---
+++ leet-code/easy/2300-2399/2357-make-array-zero-by-subtracting-equal-amounts.py
@@ -0,0 +1,12 @@
+# You are given a non-negative integer array nums. In one operation, you must:
+#
+# Choose a positive integer x such that x is less than or equal to the smallest non-zero element in nums.
+# Subtract x from every positive element in nums.
+# Return the minimum number of operations to make every element in nums equal to 0.
+
+from typing import List
+
+
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        return len(set(nums) - {0})
