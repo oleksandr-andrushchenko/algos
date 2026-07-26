136551928873dcef8841e96ec4394c01c260d1dc
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-07-26 12:38:02 -0500

    lc: 2566-maximum-difference-by-remapping-a-digit

---
+++ leet-code/easy/2500-2599/2566-maximum-difference-by-remapping-a-digit.py
@@ -0,0 +1,23 @@
+# You are given an integer num. You know that Bob will sneakily remap one of the 10 possible digits (0 to 9) to another
+# digit.
+#
+# Return the difference between the maximum and minimum values Bob can make by remapping exactly one digit in num.
+#
+# Notes:
+#
+# When Bob remaps a digit d1 to another digit d2, Bob replaces all occurrences of d1 in num with d2.
+# Bob can remap a digit to itself, in which case num does not change.
+# Bob can remap different digits for obtaining minimum and maximum values respectively.
+# The resulting number after remapping can contain leading zeroes.
+
+class Solution:
+    def minMaxDifference(self, num: int) -> int:
+        digits = str(num)
+
+        max_digit = next((digit for digit in digits if digit != "9"), "9")
+        maximum = int(digits.replace(max_digit, "9"))
+
+        min_digit = digits[0]
+        minimum = int(digits.replace(min_digit, "0"))
+
+        return maximum - minimum
