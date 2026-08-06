b7221ca5d245be99dab56dee3e21c82f11310d2b
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-08-06 11:21:11 -0500

    lc: 2656-maximum-sum-with-exactly-k-elements

---
+++ leet-code/easy/2600-2699/2656-maximum-sum-with-exactly-k-elements.ts
@@ -0,0 +1,14 @@
+// You are given a 0-indexed integer array nums and an integer k. Your task is to perform the following operation
+// exactly k times in order to maximize your score:
+//
+// Select an element m from nums.
+// Remove the selected element m from the array.
+// Add a new element with a value of m + 1 to the array.
+// Increase your score by m.
+// Return the maximum score you can achieve after performing the operation exactly k times.
+
+function maximizeSum(nums: number[], k: number): number {
+  const maxValue = Math.max(...nums);
+
+  return k * maxValue + (k * (k - 1)) / 2;
+}
