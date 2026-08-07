6ddecc5a7ecf2c6a2fb9786b47ceb07769af584c
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-08-07 11:59:50 -0500

    lc: 2667-create-hello-world-function

---
+++ leet-code/easy/2600-2699/2667-create-hello-world-function.ts
@@ -0,0 +1,12 @@
+// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+
+function createHelloWorld() {
+  return function (...args): string {
+    return "Hello World";
+  };
+}
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
