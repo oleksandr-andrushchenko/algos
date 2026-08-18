f8b48718d0273f951ef66a8c7019d756f6981de7
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-08-18 13:30:15 -0500

    lc: 2769-find-the-maximum-achievable-number

---
+++ leet-code/easy/2700-2799/2769-find-the-maximum-achievable-number.py
@@ -0,0 +1,9 @@
+# Given two integers, num and t. A number x is achievable if it can become equal to num after applying the following
+# operation at most t times:
+#
+# Increase or decrease x by 1, and simultaneously increase or decrease num by 1.
+# Return the maximum possible value of x.
+
+class Solution:
+    def theMaximumAchievableX(self, num: int, t: int) -> int:
+        return num + 2 * t
