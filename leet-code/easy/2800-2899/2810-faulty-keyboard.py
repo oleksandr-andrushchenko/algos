cffb447842d7198bab73a4c1010f01e3a2149813
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-08-20 10:58:46 -0500

    lc: 2810-faulty-keyboard

---
+++ leet-code/easy/2800-2899/2810-faulty-keyboard.py
@@ -0,0 +1,18 @@
+# Your laptop keyboard is faulty, and whenever you type a character 'i' on it, it reverses the string that you have
+# written. Typing other characters works as expected.
+#
+# You are given a 0-indexed string s, and you type each character of s using your faulty keyboard.
+#
+# Return the final string that will be present on your laptop screen.
+
+class Solution:
+    def finalString(self, s: str) -> str:
+        result = []
+
+        for char in s:
+            if char == "i":
+                result.reverse()
+            else:
+                result.append(char)
+
+        return "".join(result)
