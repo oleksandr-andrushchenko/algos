cc5bf25453b24dbbb17c31820c3f252b15bda028
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-08-23 08:49:49 -0500

    lc: 2833-furthest-point-from-origin

---
+++ leet-code/easy/2800-2899/2833-furthest-point-from-origin.py
@@ -0,0 +1,16 @@
+# You are given a string moves of length n consisting only of characters 'L', 'R', and '_'. The string represents your
+# movement on a number line starting from the origin 0.
+#
+# In the ith move, you can choose one of the following directions:
+#
+# move to the left if moves[i] = 'L' or moves[i] = '_'
+# move to the right if moves[i] = 'R' or moves[i] = '_'
+# Return the distance from the origin of the furthest point you can get to after n moves.
+
+class Solution:
+    def furthestDistanceFromOrigin(self, moves: str) -> int:
+        left = moves.count("L")
+        right = moves.count("R")
+        wildcards = moves.count("_")
+
+        return abs(left - right) + wildcards
