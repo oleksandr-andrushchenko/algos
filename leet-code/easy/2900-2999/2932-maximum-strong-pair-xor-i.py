764ca9832bbd3362819c7f74e203ee585967ef0c
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-09-01 11:26:07 -0500

    lc: 2932-maximum-strong-pair-xor-i

---
+++ leet-code/easy/2900-2999/2932-maximum-strong-pair-xor-i.py
@@ -0,0 +1,24 @@
+# You are given a 0-indexed integer array nums. A pair of integers x and y is called a strong pair if it satisfies the
+# condition:
+#
+# |x - y| <= min(x, y)
+# You need to select two integers from nums such that they form a strong pair and their bitwise XOR is the maximum among
+# all strong pairs in the array.
+#
+# Return the maximum XOR value out of all possible strong pairs in the array nums.
+#
+# Note that you can pick the same integer twice to form a pair.
+
+from typing import List
+
+
+class Solution:
+    def maximumStrongPairXor(self, nums: List[int]) -> int:
+        ans = 0
+
+        for x in nums:
+            for y in nums:
+                if abs(x - y) <= min(x, y):
+                    ans = max(ans, x ^ y)
+
+        return ans
