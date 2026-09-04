400ccba3b9314cd9d240e9eb77e116d42211533a
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-09-04 16:15:53 -0500

    lc: 2951-find-the-peaks

---
+++ leet-code/easy/2900-2999/2951-find-the-peaks.py
@@ -0,0 +1,20 @@
+# You are given a 0-indexed array mountain. Your task is to find all the peaks in the mountain array.
+#
+# Return an array that consists of indices of peaks in the given array in any order.
+#
+# Notes:
+#
+# A peak is defined as an element that is strictly greater than its neighboring elements.
+# The first and last elements of the array are not a peak.
+
+from typing import List
+
+
+class Solution:
+    def findPeaks(self, mountain: List[int]) -> List[int]:
+        return [
+            i
+            for i in range(1, len(mountain) - 1)
+            if mountain[i] > mountain[i - 1]
+               and mountain[i] > mountain[i + 1]
+        ]
