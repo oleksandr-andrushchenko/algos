afe8deee587285180daf80c18cfa7f0f2aa70362
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-09-05 16:13:53 -0500

    lc: 2980-check-if-bitwise-or-has-trailing-zeros

---
+++ leet-code/easy/2900-2999/2980-check-if-bitwise-or-has-trailing-zeros.py
@@ -0,0 +1,16 @@
+# You are given an array of positive integers nums.
+#
+# You have to check if it is possible to select two or more elements in the array such that the bitwise OR of the
+# selected elements has at least one trailing zero in its binary representation.
+#
+# For example, the binary representation of 5, which is "101", does not have any trailing zeros, whereas the binary
+# representation of 4, which is "100", has two trailing zeros.
+#
+# Return true if it is possible to select two or more elements whose bitwise OR has trailing zeros, return false otherwise.
+
+from typing import List
+
+
+class Solution:
+    def hasTrailingZeros(self, nums: List[int]) -> bool:
+        return sum(num % 2 == 0 for num in nums) >= 2
