d28fc8135626fd90969bec2a1a009e2941cc52e0
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-09-06 18:17:11 -0500

    lc: 2981-find-longest-special-substring-that-occurs-thrice-i

---
+++ leet-code/easy/2900-2999/2981-find-longest-special-substring-that-occurs-thrice-i.py
@@ -0,0 +1,27 @@
+# You are given a string s that consists of lowercase English letters.
+#
+# A string is called special if it is made up of only a single character. For example, the string "abc" is not special,
+# whereas the strings "ddd", "zz", and "f" are special.
+#
+# Return the length of the longest special substring of s which occurs at least thrice, or -1 if no special substring
+# occurs at least thrice.
+#
+# A substring is a contiguous non-empty sequence of characters within a string.
+
+class Solution:
+    def maximumLength(self, s: str) -> int:
+        ans = -1
+
+        for length in range(1, len(s) + 1):
+            counts = {}
+
+            for i in range(len(s) - length + 1):
+                sub = s[i:i + length]
+
+                if len(set(sub)) == 1:
+                    counts[sub] = counts.get(sub, 0) + 1
+
+                    if counts[sub] >= 3:
+                        ans = length
+
+        return ans
