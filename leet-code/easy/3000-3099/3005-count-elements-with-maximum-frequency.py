16099fdc22ef8e09eed229dfb0b7c4c454b262d1
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-09-07 13:43:37 -0500

    lc: 3005-count-elements-with-maximum-frequency

---
+++ leet-code/easy/3000-3099/3005-count-elements-with-maximum-frequency.py
@@ -0,0 +1,20 @@
+# You are given an array nums consisting of positive integers.
+#
+# Return the total frequencies of elements in nums such that those elements all have the maximum frequency.
+#
+# The frequency of an element is the number of occurrences of that element in the array.
+
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def maxFrequencyElements(self, nums: List[int]) -> int:
+        counts = Counter(nums)
+        max_frequency = max(counts.values())
+
+        return sum(
+            frequency
+            for frequency in counts.values()
+            if frequency == max_frequency
+        )
