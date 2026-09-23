bd2f987c3f09af0f5b1450fb3451ecf571aacdf8
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-09-23 13:25:30 -0500

    lc: 3194-minimum-average-of-smallest-and-largest-elements

---
+++ leet-code/easy/3100-3199/3194-minimum-average-of-smallest-and-largest-elements.py
@@ -0,0 +1,22 @@
+# You have an array of floating point numbers averages which is initially empty. You are given an array nums of n
+# integers where n is even.
+#
+# You repeat the following procedure n / 2 times:
+#
+# Remove the smallest element, minElement, and the largest element maxElement, from nums.
+# Add (minElement + maxElement) / 2 to averages.
+# Return the minimum element in averages.
+
+from typing import List
+
+
+class Solution:
+    def minimumAverage(self, nums: List[int]) -> float:
+        nums.sort()
+        result = float("inf")
+
+        for i in range(len(nums) // 2):
+            average = (nums[i] + nums[-i - 1]) / 2
+            result = min(result, average)
+
+        return result
