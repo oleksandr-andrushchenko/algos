a0184adb81f6659f80a9288b36e5404c3828b87d
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-09-23 13:27:55 -0500

    lc: 3206-alternating-groups-i

---
+++ leet-code/easy/3200-3299/3206-alternating-groups-i.py
@@ -0,0 +1,29 @@
+# There is a circle of red and blue tiles. You are given an array of integers colors. The color of tile i is represented
+# by colors[i]:
+#
+# colors[i] == 0 means that tile i is red.
+# colors[i] == 1 means that tile i is blue.
+# Every 3 contiguous tiles in the circle with alternating colors (the middle tile has a different color from its left
+# and right tiles) is called an alternating group.
+#
+# Return the number of alternating groups.
+#
+# Note that since colors represents a circle, the first and the last tiles are considered to be next to each other.
+
+from typing import List
+
+
+class Solution:
+    def numberOfAlternatingGroups(self, colors: List[int]) -> int:
+        n = len(colors)
+        result = 0
+
+        for i in range(n):
+            left = colors[i]
+            middle = colors[(i + 1) % n]
+            right = colors[(i + 2) % n]
+
+            if left != middle and middle != right:
+                result += 1
+
+        return result
