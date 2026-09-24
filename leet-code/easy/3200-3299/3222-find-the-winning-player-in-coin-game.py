9d79a96c78d338d8a0a10cbb542493646c1f52b0
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-09-24 12:16:48 -0500

    lc: 3222-find-the-winning-player-in-coin-game

---
+++ leet-code/easy/3200-3299/3222-find-the-winning-player-in-coin-game.py
@@ -0,0 +1,12 @@
+# You are given two positive integers x and y, denoting the number of coins with values 75 and 10 respectively.
+#
+# Alice and Bob are playing a game. Each turn, starting with Alice, the player must pick up coins with a total value 115.
+# If the player is unable to do so, they lose the game.
+#
+# Return the name of the player who wins the game if both players play optimally.
+
+class Solution:
+    def winningPlayer(self, x: int, y: int) -> str:
+        turns = min(x, y // 4)
+
+        return "Alice" if turns % 2 else "Bob"
