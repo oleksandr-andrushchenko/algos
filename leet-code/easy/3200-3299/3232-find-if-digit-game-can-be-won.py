2a6fba8cc7f71cfa75c5f3e8c4341d77b9231856
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-09-26 11:40:30 -0500

    lc: 3232-find-if-digit-game-can-be-won

---
+++ leet-code/easy/3200-3299/3232-find-if-digit-game-can-be-won.py
@@ -0,0 +1,17 @@
+# You are given an array of positive integers nums.
+#
+# Alice and Bob are playing a game. In the game, Alice can choose either all single-digit numbers or all double-digit
+# numbers from nums, and the rest of the numbers are given to Bob. Alice wins if the sum of her numbers is strictly
+# greater than the sum of Bob's numbers.
+#
+# Return true if Alice can win this game, otherwise, return false.
+
+from typing import List
+
+
+class Solution:
+    def canAliceWin(self, nums: List[int]) -> bool:
+        single = sum(num for num in nums if num < 10)
+        double = sum(num for num in nums if num >= 10)
+
+        return single != double
